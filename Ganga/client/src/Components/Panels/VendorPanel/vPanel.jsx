d96184a5e904785417f0db66f4e0a3f00393c93b
Carlos Medina <crmg.camg@gmail.com>
2021-12-05 17:31:55 -0300

    algo

--- Ganga/client/src/Components/Panels/VendorPanel/vPanel.jsx
+++ Ganga/client/src/Components/Panels/VendorPanel/vPanel.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React, { useState } from "react";
 
 import Nav from "../../Nav/NavBar/nav";
@@ -19,6 +20,75 @@ export default function VendorPanel() {
         <div className={s.container}>
           <VendorSidebar productos={productos} verProductos={verProductos} />
           <div className={s.body}></div>
+=======
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Nav from "../../Nav/NavBar/nav";
+import VendedorSidebar from "./vSidebar";
+import InfoUser from './infoUser'
+import s from './vPanel.module.css'
+
+export default function VendorPanel() {
+    const dispatch = useDispatch();
+    const [datos, verDatos] = useState(false);
+    const [productos, verProductos] = useState(false);
+    const [vendedores, verVendedores] = useState(false);
+
+    return (
+        <div className="bg-gray-100">
+            <Nav />
+            {!vendedores ?
+                <div>
+                    {!productos ?
+                        <div>
+                            {!datos ? (
+                                <div className={s.container}>
+                                    <div>
+                                        <VendedorSidebar datos={datos} productos={productos}
+                                            verProductos={verProductos} vendedores={vendedores} verVendedores={verVendedores} />
+                                    </div>
+                                    <div className={s.body}>
+                                        <h4 className="text-3xl text-center font-light font-serif p-5">
+                                            Info
+                                        </h4>
+<InfoUser/>
+                                        <div className={s.newInfo}>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            ) : (
+                                <div className={s.containerUsuarios}>
+                                    <div>
+                                        <h1>Holaaa</h1>
+                                    </div>
+                                    <div className={s.usuariosBody}>
+                                        <p>HOLAAA</p>
+                                    </div>
+                                </div>
+                            )}
+                        </div>
+                        :
+                        <div className={s.containerProductos}>
+                            <div>
+                                <p>Hola</p>
+                            </div>
+                            <div className={s.productosBody}>
+                                <p>HOLA</p>
+                            </div>
+                        </div>
+                    }
+                </div>
+                :
+                <div className={s.containerVendedores}>
+                    <div>
+                        <h2>knfelinfpi</h2>
+                    </div>
+                    <div className={s.vendedoresBody}>
+                        <h1>fenbkufbd</h1>
+                    </div>
+                </div>}
+>>>>>>> Stashed changes
         </div>
       )}
     </div>
--- Ganga/client/src/Components/Panels/VendorPanel/vSidebar.jsx
+++ Ganga/client/src/Components/Panels/VendorPanel/vSidebar.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from 'react';
 import s from './vendor.module.css';
 
@@ -24,4 +25,62 @@ export default function VendorSidebar({productos, verProductos}) {
             </ul>
         </div>
     )
+=======
+import React from "react";
+import s from "./vSidebar.module.css";
+
+export default function VendedorSidebar({
+  usuarios,
+  verUsuarios,
+  productos,
+  verProductos,
+  vendedores,
+  verVendedores,
+}) {
+  const toggle = () => {
+    verUsuarios((usuarios = true));
+    verProductos((productos = false));
+    verVendedores((vendedores = false));
+  };
+
+  const Toggle = () => {
+    verProductos((productos = true));
+    verUsuarios((usuarios = false));
+    verVendedores((vendedores = false));
+  };
+
+  const togle = () => {
+    verVendedores((vendedores = true));
+    verUsuarios((usuarios = false));
+    verProductos((productos = false));
+  };
+
+  const Togle = () => {
+    verVendedores((vendedores = false));
+    verProductos((productos = false));
+    verUsuarios((usuarios = false));
+  };
+
+  return (
+    <div className={s.sidebar}>
+      <div className="">
+        <h3 className="p-10 pt-32 pl-16 text-3xl opacity-60">Opciones</h3>
+        <ul>
+          <li className="p-8 pr-48 text-xl hover:bg-gray-400 active:bg-gray-500">
+            <button onClick={Togle}>Tus Datos</button>
+          </li>
+          <li className="p-8 text-xl hover:bg-gray-400 active:bg-gray-500">
+            <button onClick={toggle}>Tus Productos</button>
+          </li>
+          <li className="p-8 text-xl hover:bg-gray-400 active:bg-gray-500">
+            <button onClick={Toggle}>Tus Ventas</button>
+          </li>
+          <li className="p-8 text-xl hover:bg-gray-400 active:bg-gray-500">
+            <button onClick={togle}>Otro</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+>>>>>>> Stashed changes
 }
