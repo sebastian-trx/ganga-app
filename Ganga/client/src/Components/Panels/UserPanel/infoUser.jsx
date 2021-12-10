fc85480c8a8253e2eb68f2a71c39a4c1a7f4230f
Ezequiel Camargo <ezequielcamargo009@gmail.com>
2021-12-10 00:41:53 -0300

    Modificar user uptadetetete
    -Se modifica el model de user, propiedad de "cellphone", ahora es BIGINT
    -Se agregan nuevos inputs para modificar
    -Se modifica el "sessionActive" para que llegue toda la info del user para el front

--- Ganga/client/src/Components/Panels/UserPanel/infoUser.jsx
+++ Ganga/client/src/Components/Panels/UserPanel/infoUser.jsx
@@ -1,36 +1,46 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useNavigate} from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import { updateUser, getUserInfoGoogle } from "../../Redux/Actions/actions";
 import s from './user.module.css';
 
 export default function InfoUser() {
-    
+
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const user = useSelector((state) => state.getInfoGoogle);
 
     useEffect(() => {
         dispatch(getUserInfoGoogle());
-      }, [dispatch]);
+    }, [dispatch]);
 
-      const name = user.name
-      const surname = user.surname
-      const mail = user.mail
-      // const birthdate = userfil.birthdate
-      const address = user.address
-  
-      const [input, setInput] = useState({
-          id: user.id,
-          name: name,
-          surname: surname,
-          mail: mail,
-          // birthdate: birthdate,
-          address: address,
-      })
+    const name = user.name;
+    const surname = user.surname;
+    const mail = user.mail;
+    const cellphone = user.cellphone;
+    const birthdate = user.birthdate;
+    const address = user.address;
+    const country = user.country;
+    const province = user.province;
+    const cp = user.cp;
+    // const image = user.image;
 
-      const handleChange = (e) => {
+    const [input, setInput] = useState({
+        id: user.id,
+        name: name,
+        surname: surname,
+        mail: mail,
+        cellphone: cellphone,
+        birthdate: birthdate,
+        address: address,
+        country: country,
+        province: province,
+        cp: cp,
+        // image: image,
+    })
+
+    const handleChange = (e) => {
         setInput({
             ...input,
             [e.target.name]: e.target.value
@@ -44,76 +54,127 @@ export default function InfoUser() {
         window.location.reload();
     }
 
-  
+
     return (
         <div>
-        <h3 className="text-center text-3xl m-5 mt-20">Modificar tus datos</h3>
-        <form className="text-center" onSubmit={submit}>
-            <div className="p-5">
-                <div>
-                    <label> Nombre </label>
+            <h3 className="text-center text-3xl m-5 mt-20">Modificar tus datos</h3>
+            <form className="text-center" onSubmit={submit}>
+                <div className="p-4">
+                    <div>
+                        <label> Nombre </label>
+                    </div>
+                    <input className="text-center bg-gray-700 text-white"
+                        onChange={handleChange}
+                        type="text"
+                        value={input.name}
+                        name="name"
+                        placeholder="Nombre"
+                    />
                 </div>
-                <input className="text-center bg-gray-700 text-white"
-                    onChange={handleChange}
-                    type="text"
-                    value={input.name}
-                    name="name"
-                    placeholder="Nombre"
-                />
-            </div>
-            <div className="p-5">
-                <div>
-                    <label> Apellido </label>
+                <div className="p-4">
+                    <div>
+                        <label> Apellido </label>
+                    </div>
+                    <input className="text-center bg-gray-700 text-white"
+                        onChange={handleChange}
+                        type="text"
+                        value={input.surname}
+                        name="surname"
+                        placeholder="Apellido"
+                    />
                 </div>
-                <input className="text-center bg-gray-700 text-white"
-                    onChange={handleChange}
-                    type="text"
-                    value={input.surname}
-                    name="surname"
-                    placeholder="Apellido"
-                />
-            </div>
-            <div className="p-5">
-                <div>
-                    <label> Correo </label>
+                <div className="p-4">
+                    <div>
+                        <label> Correo </label>
+                    </div>
+                    <input className="text-center bg-gray-700 text-white"
+                        onChange={handleChange}
+                        type="text"
+                        value={input.mail}
+                        name="mail"
+                        placeholder="Correo"
+                    />
                 </div>
-                <input className="text-center bg-gray-700 text-white"
-                    onChange={handleChange}
-                    type="text"
-                    value={input.mail}
-                    name="mail"
-                    placeholder="Correo"
-                />
-            </div>
-            {/* <div>
-                <input
-                    onChange={handleChange}
-                    type="date"
-                    value={input.birthdate}
-                    name="birthdate"
-                    placeholder="Fecha de Nacimiento"
-                />
-            </div> */}
-            <div className="p-5">
-                <div>
-                    <label> Zona </label>
+                <div className="p-4">
+                    <div>
+                        <label> Número de celular </label>
+                    </div>
+                    <input className="text-center bg-gray-700 text-white"
+                        onChange={handleChange}
+                        type="number"
+                        value={input.cellphone}
+                        name="cellphone"
+                        placeholder="Núnero de celular"
+                    />
                 </div>
-                <input className="text-center bg-gray-700 text-white"
-                    onChange={handleChange}
-                    type="text"
-                    value={input.address}
-                    name="address"
-                    placeholder="Zona"
-                />
-            </div>
-            <div className="p-5">
-                <button className={s.btnActualizar} type="submit">
-                    Actualizar
-                </button>
-            </div>
+                <div className="p-4">
+                    <div>
+                        <label> Fecha de Nacimiento </label>
+                    </div>
+                    <input className="text-center bg-gray-700 text-white"
+                        onChange={handleChange}
+                        type="date"
+                        value={input.birthdate}
+                        name="birthdate"
+                        placeholder="Fecha de Nacimiento"
+                    />
+                </div>
+                <div className="p-4">
+                    <div>
+                        <label> Dirección </label>
+                    </div>
+                    <input className="text-center bg-gray-700 text-white"
+                        onChange={handleChange}
+                        type="text"
+                        value={input.address}
+                        name="address"
+                        placeholder="Dirección"
+                    />
+                </div>
+                <div className="p-4">
+                    <div>
+                        <label> País </label>
+                    </div>
+                    <input className="text-center bg-gray-700 text-white"
+                        onChange={handleChange}
+                        type="text"
+                        value={input.country}
+                        name="country"
+                        placeholder="País"
+                    />
+                </div>
+                <div className="p-4">
+                    <div>
+                        <label> Provincia </label>
+                    </div>
+                    <input className="text-center bg-gray-700 text-white"
+                        onChange={handleChange}
+                        type="text"
+                        value={input.province}
+                        name="province"
+                        placeholder="Provincia"
+                    />
+                </div>
+                <div className="p-4">
+                    <div>
+                        <label> Código Postal </label>
+                    </div>
+                    <input className="text-center bg-gray-700 text-white"
+                        onChange={handleChange}
+                        type="text"
+                        value={input.cp}
+                        name="cp"
+                        placeholder="Código postal"
+                    />
+                </div>
+                <div className="p-5">
+                    <button className={s.btnActualizar} type="submit">
+                        Actualizar
+                    </button>
+                </div>
 
-        </form>
-    </div>
+            </form>
+        </div>
 
     )
 }
