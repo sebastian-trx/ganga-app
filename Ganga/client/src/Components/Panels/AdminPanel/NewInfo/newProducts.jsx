b7c0ac7db023b5d9943d285a304947301df24482
Jero Caride <75335780+mapachus@users.noreply.github.com>
2021-12-11 19:08:45 -0300

    actualizacion paneles

---
+++ Ganga/client/src/Components/Panels/AdminPanel/NewInfo/newProducts.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import Card from '../../../Card/card';
+
+export default function NewProducts({products}) {
+    let newProducts = products.filter(p=> p.approved === false);
+    console.log("np", newProducts);
+    return (
+        <div>
+        <h1> Productos a revisar:</h1>
+            {
+                newProducts.map(p => {
+                    return (
+                        <div className='inline-block'>
+                        <Card
+                        name= {p.name} image={p.image} id={p.id} price={p.price}
+                        />
+                     </div>
+                        
+                )
+                })
+            }
+        </div>
+    )
+}
