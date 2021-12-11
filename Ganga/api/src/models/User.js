cf55af11a086f2ad7c003bdbf89ab0303fda2c76
rensoolariaga <renso.o.1996@gmail.com>
2021-12-10 22:46:15 -0300

    officialStore working, fix the filter with this prop & prop approve in products for superAdmin panel

--- Ganga/api/src/models/User.js
+++ Ganga/api/src/models/User.js
@@ -76,6 +76,11 @@ module.exports = (sequelize) => {
                 defaultValue: [],
                 // allowNull: false,
               },
+            
+              officialStore: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false
+              }
 
         },
         { timestamps: true, paranoid: true }
