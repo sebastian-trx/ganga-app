a73459b2a03c8c72a81db4e75d6b5fc408805fa7
Jairo Colón <jaicodigital@gmail.com>
2021-12-14 23:10:13 -0500

    SC Estilos Categoriass
    Agrego todos los estilos de categoria y sub categorias

--- Ganga/client/src/Components/Nav/Categories/categories.jsx
+++ Ganga/client/src/Components/Nav/Categories/categories.jsx
@@ -1,77 +1,66 @@
 import React, { useState, useEffect } from "react";
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams, useNavigate, Link } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams, useNavigate } from "react-router-dom";
 import Card from "../../Card/card";
-import { orderByPrice, getUser, getSubCategoryByName, getProduct, getFilterByCategory, filterBySubCat } from "../../Redux/Actions/actions";
-import s from './categories.module.css'
-import FilterPrice from "../Filter/filterPrice";
-import Pagination from '../../Home/Pagination/pagination'
-import { IoIosCart } from "react-icons/io";
-
-import Logo from "../Logo/logo";
-import User from '../User/user'
-import { VscDebugRestart } from "react-icons/vsc";
-
-// let PageSize = 10;
+import {
+  orderByPrice,
+  getUser,
+  getSubCategoryByName,
+  getProduct,
+  getFilterByCategory,
+  filterBySubCat,
+} from "../../Redux/Actions/actions";
+import "./categories.css";
+import FilterPrice2 from "../Filter/filterPrice2";
+import Pagination from "../../Home/Pagination/pagination";
+import Nav from "../NavBar/nav";
+import download2 from "../../Resources/cargando.gif";
+import FooterCategory from "../../Home/Footer/footerCategory";
 
 export default function Categorias() {
-
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const { nombre } = useParams();
   const subcategories = useSelector((state) => state.subcategories);
-  const userGoogle = useSelector((state) => state.getInfoGoogle);
   const allProduct = useSelector((state) => state.product);
   const [, setOrden] = useState("");
 
-  // - - - Paginado anterior - - - //
-
   const [currentPage, setCurrentPage] = useState(1);
-const [elementsPerPage, /*setElementsPerPage*/] = useState(12)
+  const [elementsPerPage] = useState(12);
   const indexOfLastProducts = currentPage * elementsPerPage;
   const indexOfFirstProducts = indexOfLastProducts - elementsPerPage;
-  const currentProducts =allProduct?.slice(indexOfFirstProducts, indexOfLastProducts);
+  const currentProducts = allProduct?.slice(
+    indexOfFirstProducts,
+    indexOfLastProducts
+  );
 
   const paginate = (pageNumbers) => {
-    setCurrentPage(pageNumbers)
-  }
-
-  // - - - Paginado nuevo - - - //
-
-  // const [currentPage, setCurrentPage] = useState(1);
-
-  // const currentProducts = useMemo(() => {
-  //   const firstPageIndex = (currentPage - 1) * PageSize;
-  //   const lastPageIndex = firstPageIndex + PageSize;
-  //   return allProduct.slice(firstPageIndex, lastPageIndex);
-  // }, [currentPage]);
-
-  // - - - Paginado nuevo - - - //
+    setCurrentPage(pageNumbers);
+  };
 
   useEffect(() => {
-    dispatch(getFilterByCategory(nombre))
-  }, [dispatch, nombre])
+    dispatch(getFilterByCategory(nombre));
+  }, [dispatch, nombre]);
 
   useEffect(() => {
     dispatch(getSubCategoryByName(nombre));
-  }, [dispatch, nombre])
+  }, [dispatch, nombre]);
 
   useEffect(() => {
-    dispatch(getUser())
-  }, [dispatch])
+    dispatch(getUser());
+  }, [dispatch]);
 
   function handleClick(e) {
     e.preventDefault();
-    navigate("/catalogo")
-    dispatch(getProduct())
+    navigate("/catalogo");
+    dispatch(getProduct());
   }
 
   function handleSubCat(e) {
     e.preventDefault();
-    dispatch(filterBySubCat(e.target.value))
+    dispatch(filterBySubCat(e.target.value));
     setCurrentPage(1);
-
   }
 
   function handleOrder(e) {
@@ -82,106 +71,71 @@ const [elementsPerPage, /*setElementsPerPage*/] = useState(12)
   }
 
   return (
-    <div>
-      <nav className="flex justify-between items-center h-20  text-black">
-        <Link to="/" className="pl-10">
-          <div className=" w-30">
-            <Logo />
+    <div className="containerCategory">
+      <Nav className="flex justify-between items-center h-20 w-8 text-black" />
+      <div className="bannerProdCategory">
+        <h1 className="aloneCategory">SOLO AQUÍ EN GANGA</h1>
+      </div>
+      <div className="infCategory">
+        <div className="filtrosCategory">
+          <div className="conNameFilCategory">
+            <h1 className="nameFilCategory">FILTROS:</h1>
           </div>
-        </Link>
-
-        <div>
-          <select className="w-40" onChange={handleSubCat}>
-            <option > SubCategorias </option>
-            {
-              subcategories[0]?.subcategories.map((el, i) =>
-              (
+          <div className="inputContainerCategory">
+            <FilterPrice2 />
+          </div>
+          <div className="selectFilterCategory">
+            <select className="selectCategory" onChange={(e) => handleOrder(e)}>
+              <option> ordena por precio: </option>
+              <option value="Menor-Mayor"> Mayor a Menor </option>
+              <option value="Mayor-Menor"> Menor a Mayor </option>
+            </select>
+          </div>
+          <div className="selectFilterSubCategory">
+            <select className="selectSubCategory" onChange={handleSubCat}>
+              <option>elije por sub categoria</option>
+              {subcategories[0]?.subcategories.map((el, i) => (
                 <option key={i} value={el}>
                   {el}
                 </option>
-              ))
-            }
-          </select>
+              ))}
+            </select>
+          </div>
+          <div className="conBorCategory">
+            <button className="borCategory" onClick={handleClick}>
+              Borra filtros
+            </button>
+          </div>
         </div>
-
-
-        <div className=" w-30">
-          <button onClick={handleClick}>
-            <VscDebugRestart />
-          </button>
-        </div>
-
-
-
-        <div className="px-6">
-          <FilterPrice />
-        </div>
-
-
-
-        <div className="pr-10">
-          <select className="w-38" onChange={(e) => handleOrder(e)}>
-            <option value="All"> Orden por Precio: </option>
-            <option value="Menor-Mayor"> Mayor a Menor </option>
-            <option value="Mayor-Menor"> Menor a Mayor </option>
-          </select>
-        </div>
-
-
-        <Link to="/carrito" className="pl-6 pr-10">
-          <button>
-            <IoIosCart />
-          </button>
-        </Link>
-        {
-          userGoogle && userGoogle.login ?
-            <User /> :
-            <>
-              <Link to="/registrarme" className="pl-4">
-                <span>Crear cuenta</span>
-              </Link>
-
-              <Link to="/ingresar" className="pl-4">
-                <span>Iniciar Sesión</span>
-              </Link>
-            </>
-        }
-      </nav>
-
-      <div className={s.cards}>
+      </div>
+      <div className="containerCardsCategory">
         {currentProducts?.length === 0 ? (
-          <div>
-            <h1>Cargando...</h1>
+          <div className="download2">
+            <img src={download2} alt="" />
           </div>
         ) : (
           currentProducts?.map((el, i) => {
             return (
-              <div key={"card" + i}>
-                <Card
-                  name={el.name}
-                  image={el.image}
-                  price={el.price}
-                  id={el.id}
-                />
+              <div>
+                <div className="cardProductsCategory" key={"card" + i}>
+                  <Card
+                    name={el.name}
+                    image={el.image}
+                    price={el.price}
+                    id={el.id}
+                  />
+                </div>
               </div>
             );
           })
         )}
       </div>
-      {/* <div>
-        <Pagination
-          currentPage={currentPage}
-          totalCount={allProduct.length}
-          pageSize={PageSize}
-          onPageChange={page => setCurrentPage(page)}
-        />
-      </div> */}
-      <div>
-        <Pagination
-          elementsPerPage={elementsPerPage}
-          allProduct={allProduct}
-          paginate={paginate} />
-      </div>
+      <Pagination
+        elementsPerPage={elementsPerPage}
+        allProduct={allProduct}
+        paginate={paginate}
+      />
+      <FooterCategory />
     </div>
   );
 }
