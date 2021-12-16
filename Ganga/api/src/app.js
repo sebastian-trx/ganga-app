ec2a45d97942169ba8677eb1f9e6d75c2d5003de
Sebastián Torres <torressebastian014@gmail.com>
2021-12-16 08:03:41 -0500

    deploy

--- Ganga/api/src/app.js
+++ Ganga/api/src/app.js
@@ -3,7 +3,11 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const routes = require('./routes/index.js');
+// MongoDB
+const MongoStore = require('connect-mongo');
+const mongoose = require("mongoose");
 // passport
+
 const passport = require('passport');                   //se importa passport para autenticacion con google
 const session = require("express-session");             //se importa session para el manejo de sesiones con passport
 require('./utils/google-passport-setup');              //se importa para toda la app, la estructura de passport con google
@@ -15,28 +19,38 @@ const server = express();
 
 const cors = require('cors');
 
+server.set("trust proxy", 1);
+
 server.name = 'API';
 
 // server.use(cors());
-// server.use(
-//   cors({
-//     origin: true,             //se habilitan las credenciales de cors para los pedidos que vengan del front
-//     credentials: true,
-//   })
-// )
+server.use(
+  cors({
+    origin: "https://ganga-app.vercel.app",             //se habilitan las credenciales de cors para los pedidos que vengan del front
+    credentials: true,
+  })
+)
 
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+// server.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://ganga-app.vercel.app'); // update to match the domain you will make the request from
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//   next();
+// });
 
+mongoose.connect(
+  "mongodb+srv://eze:eze@cluster0.5cxnx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Mongoose Is Connected");
+  }
+);
+// mongoose.set("useCreateIndex", false);
 
 // - - - - Deploy - - - -
 
@@ -49,17 +63,34 @@ server.use((req, res, next) => {
 //       httpOnly: true,
 //       secure: true,
 //       sameSite: "none",
-//     }
+//     },
+//     store: MongoStore.create({
+//       mongoUrl: "mongodb+srv://eze:eze@cluster0.5cxnx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+//       ttl: 14 * 24 * 60 * 60 // = 14 days. Default
+//     })
 //   })
 // );
 
-server.use(                                             //se habilita el manejo de sesiones para el server
+server.use(
   session({
     secret: "secretcode",
-    resave: true,
+    resave: false,
+    path: "/",
+    proxy: true,
     saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: "mongodb+srv://eze:eze@cluster0.5cxnx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+      ttl: 14 * 24 * 60 * 60 // = 14 days. Default
+    }),
+    cookie: {
+      
+      sameSite: 'none',
+      secure: true,
+      maxAge: 60 * 60 * 1000 * 24 * 365,
+    },
   })
 );
+
 
 server.use(passport.initialize());                    //se inicializa passport y passport session para el manejo de la session con passport
 server.use(passport.session());
